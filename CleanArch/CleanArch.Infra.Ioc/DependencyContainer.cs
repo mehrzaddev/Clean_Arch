4542809b87d298af31445ff9fd917561349bcdf7
mhrxadev <60345056+mhrxadev@users.noreply.github.com>
2020-04-18 19:10:57 +0430

    Create DependenycContainer in CleanArch.Infra.Ioc

---
+++ CleanArch/CleanArch.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Application Layer
+            service.AddScoped<ICourseService, CourseService>();
+
+            //Infra Data Layer
+            service.AddScoped<ICourseRepository, CourseReository>();
+        }
+    }
+}
